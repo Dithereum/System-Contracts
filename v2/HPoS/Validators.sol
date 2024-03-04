4562e6fe512b7498751bb44b30d58b378e425e05
Vaishali Patel <46371938+vaishalihpatel@users.noreply.github.com>
2024-03-04 09:35:16 +0530

    Update Validators.sol
    Update import path

--- v2/HPoS/Validators.sol
+++ v2/HPoS/Validators.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "Params.sol";
-import "Proposal.sol";
-import "Punish.sol";
+import "./Params.sol";
+import "./Proposal.sol";
+import "./Punish.sol";
 
 contract Validators is Params {
 
