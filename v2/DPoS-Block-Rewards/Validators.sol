86fb8f662f4f2277a9bfeca4369c76d852f90064
Vaishali Patel <46371938+vaishalihpatel@users.noreply.github.com>
2024-03-04 09:33:04 +0530

    Update Validators.sol
    Update import path

--- v2/DPoS-Block-Rewards/Validators.sol
+++ v2/DPoS-Block-Rewards/Validators.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "Params.sol";
-import "Punish.sol";
+import "./Params.sol";
+import "./Punish.sol";
 
 contract Validators is Params {
 
